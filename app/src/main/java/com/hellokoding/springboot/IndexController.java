6cad1b65fd94bb96719af9dc713efc4259f78ebd
yennanliu <f339339@gmail.com>
2024-02-09 12:43:37 +0800

    add endpoint, add port in conf

--- app/src/main/java/com/hellokoding/springboot/IndexController.java
+++ app/src/main/java/com/hellokoding/springboot/IndexController.java
@@ -11,4 +11,10 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/test")
+    public String index2() {
+        return "index";
+    }
+
 }
